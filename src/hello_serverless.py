322abe4c742b65746ec41ab77573fc276c298ff7
inputx <inputx@goodays.com>
2018-09-01 22:15:00 +1000

    update cicd deploy setting handler name

--- src/hello_serverless.py
+++ src/hello_serverless.py
@@ -4,7 +4,7 @@ from faker import Faker
 def handler(event, context):
 	faker = Faker()
 	return {
-		'message': 'This is just hello world! ' + faker.name()
+		'message': 'This is just hello world! ()' + faker.name()
 	}
 
 # print(handler([], ''))
